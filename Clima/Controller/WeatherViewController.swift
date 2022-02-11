2332756751cd8a9481e128492505552948500196
Gospodi <gospodijazzet@gmail.com>
2022-02-11 15:47:05 +0300

    Добавлен функционал по обновлению в строке поиска, если пользователем не набран текст, при нажатии кнопки поиск отображается надпись - 'введите что-либо'

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -33,6 +33,15 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
     func textFieldDidEndEditing(_ textField: UITextField) {
         searchTextField.text = ""
     }
