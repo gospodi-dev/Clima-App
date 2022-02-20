1b886946d1d01bd41e9a3d1a71a0619a0c41bb30
Gospodi <gospodijazzet@gmail.com>
2022-02-20 21:09:22 +0300

    Исправлен метод получения и сохранения файла JSON

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -17,17 +17,8 @@ struct WeatherManager {
     }
     
     func performRequest(urlString: String) {
-        
-        // 1. Create a URL
-        
         if let url = URL(string: urlString) {
-            // 2. Create a URLSession
-            
             let session = URLSession(configuration: .default)
-            
-            // 3. Give the session a task
-            
-            
             let task = session.dataTask(with: url) { (data, response, error) in
                 if error != nil {
                     print(error!)
@@ -35,15 +26,16 @@ struct WeatherManager {
                 }
                 
                 if let safeData = data {
-                    let dataString = String(data: safeData, encoding: .utf8)
-                    print(dataString)
+                    self.parseJSON(weatherData: safeData)
                 }
             }
             
-            // 4. Start the task
             task.resume()
         }
-        
+    }
+    
+    func parseJSON(weatherData: Data) {
+        <#function body#>
     }
 }
 
