2d7f672dbd1932421ef9ec3213f12771942bf391
Gospodi <gospodijazzet@gmail.com>
2022-02-21 13:56:32 +0300

    fix WeatherManager

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import CoreLocation
 
 protocol WeatherManagerDelegate {
     func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
@@ -16,11 +17,15 @@ protocol WeatherManagerDelegate {
 struct WeatherManager {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=8cb087ec1a7ba0be045896dff05ec25d&units=metric"
     
-    
     var delegate: WeatherManagerDelegate?
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(with: urlString)
+    }
+    
+    func fetchWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
+        let urlString = "\(weatherURL)&lat=\(latitude)&lon=\(longitude)"
         performRequest(with: urlString)
     }
     
@@ -58,7 +63,4 @@ struct WeatherManager {
             return nil
         }
     }
-    
-    
 }
-
